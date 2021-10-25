44a6166864259126e3918f7b03201191a247941e
Allan Burnier <allanburnier@pop-os.localdomain>
2021-10-25 22:40:21 +0200

    first commit

---
+++ src/cs107/Test.java
@@ -0,0 +1,9 @@
+package cs107;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        System.out.println(a.length);
+        System.out.println(a[2]);
+    }
+}
